0972fbd7001a0b4b0e5b2e03e16d854b5d1abfc6
Stanislav Smolevich <smolestan@Stanislavs-MacBook-Pro.local>
2019-05-16 00:10:45 +0300

    Modified Settings

--- pizza/settings.py
+++ pizza/settings.py
@@ -143,6 +143,7 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+AWS_LOCATION = 'static'
 STATIC_URL = 'https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
 
 django_heroku.settings(locals(), databases=False)
