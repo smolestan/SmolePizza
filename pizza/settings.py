66a4a4894038f4fcfdeae44c55e7efb583df13d7
Stanislav Smolevich <smolestan@Stanislavs-MacBook-Pro.local>
2019-05-15 23:27:40 +0300

    Modified Settings

--- pizza/settings.py
+++ pizza/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -141,8 +140,10 @@ AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = 'us-east-2'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
-STATIC_URL = ‘https://' + AWS_STORAGE_BUCKET_NAME + ‘.s3.amazonaws.com/’
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATIC_URL = ‘https://' + AWS_STORAGE_BUCKET_NAME + ‘.s3.amazonaws.com/’
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, “static”), )
 
 django_heroku.settings(locals(), databases=False)
