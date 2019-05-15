282e0031c1983f878e89d15f7f9dacb9bd2cec33
Stanislav Smolevich <smolestan@Stanislavs-MacBook-Pro.local>
2019-05-15 23:18:43 +0300

    Modified Settings

--- pizza/settings.py
+++ pizza/settings.py
@@ -141,6 +141,7 @@ AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = 'us-east-2'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
+STATIC_URL = ‘https://' + AWS_STORAGE_BUCKET_NAME + ‘.s3.amazonaws.com/’
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
