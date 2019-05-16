6f16ca7aebf5f6f3a27e09360cdd50f02c582872
Stanislav Smolevich <smolestan@Stanislavs-MacBook-Pro.local>
2019-05-16 15:03:04 +0300

    Modified Settings

--- pizza/settings.py
+++ pizza/settings.py
@@ -142,7 +142,7 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 AWS_QUERYSTRING_AUTH = True
 S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = S3_URL + 'static/'
+STATIC_URL = S3_URL + 'staticfiles/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
